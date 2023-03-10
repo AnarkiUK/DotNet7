96237891c349cee43ad02798cf2ae2b9ee67edae
Paul Ivey <paul@iveylabs.co.uk>
2023-03-10 23:04:13 +0000

    Done for Friday

---
+++ Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,20 @@
+﻿//#error version
+
+// Example of a raw string literal
+// The number of $ signs tells the compiler how many {} together to ignore
+string json = $$"""
+{
+    "first_name": "Paul",
+    "calculation": {{{30 + 5}}}
+}
+""";
+
+// Example of a verbatim string
+string verbatim = @"C:\t";
+
+// Example of an interpolated string
+string interp = $"My age is {30 + 5}";
+
+Console.WriteLine(json);
+Console.WriteLine(verbatim);
+Console.WriteLine(interp);
