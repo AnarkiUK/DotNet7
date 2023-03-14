d516e8afbd9a67e7b972d9588a579efb10d69700
Paul Ivey <paul@iveylabs.co.uk>
2023-03-14 19:34:56 +0000

    Chapter 2 done

---
+++ Chapter02/AsyncConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿
+HttpClient client = new();
+HttpResponseMessage response =
+    await client.GetAsync("http://www.microsoft.com");
+
+WriteLine($"Microsoft's homepage has {response.Content.Headers.ContentLength} bytes");
